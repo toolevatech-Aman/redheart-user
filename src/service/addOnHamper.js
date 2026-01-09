c0cc894a85c1b88926928510e1dca7fa9a5e0499
toolevatech-Aman <tooleva.tech@gmail.com>
2026-01-10 04:32:56 +0530

    changes

--- src/service/addOnHamper.js
+++ src/service/addOnHamper.js
@@ -3,6 +3,11 @@ import * as axiosService from "./axiosService";
 
 
 export const getHamperProduct = async (category) => {
-  const response = await axiosService.Get(`/addOn/category/${category}`);
+  const response = await axiosService.Get(`/AddOn/category/${category}`);
+  return response.data;
+};
+
+export const getAddOnProductExcept = async (category) => {
+  const response = await axiosService.Get(`/AddOn/exclude/${category}`);
   return response.data;
 };
